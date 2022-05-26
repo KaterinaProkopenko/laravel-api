1e81b6210d7a55b325b04c0a86e53b045b07d578
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-05-26 16:45:50 +0200

    Implementazione di Vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({processUrls:false});
+mix.sass('resources/sass/app.scss', 'public/css').options({processUrls:false});
+mix.js('resources/js/admin.js', 'public/js');
+mix.js('resources/js/front.js', 'public/js');
